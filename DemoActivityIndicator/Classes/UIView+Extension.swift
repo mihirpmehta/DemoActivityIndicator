9b70eab53d03b5406d08a331f641bfa3cecffedb
mihir mehta <mihir.mehta@dynamicelements.no>
2019-01-23 16:27:31 +0530

    Few fixes

---
+++ DemoActivityIndicator/Classes/UIView+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extension.swift
+//  DemoActivityIndicator
+//
+//  Created by mihir mehta on 23/01/19.
+//
+
+import Foundation
---
+++ DemoActivityIndicator/Classes/UIViewController+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+Extension.swift
+//  DemoActivityIndicator
+//
+//  Created by mihir mehta on 23/01/19.
+//
+
+import Foundation
